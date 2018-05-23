97aa8c21c576f2def02dcc1f325728a15051f4ab
Vanessa Azócar <v.azocar.adasme@gmail.com>
2018-05-23 17:00:56 -0300

    Primer boton de Nombre

---
+++ assets/app.js
@@ -0,0 +1,7 @@
+window.onload = function () {
+//primera pregunta Nombre - Bienvenida
+function OOneFunction(){
+var btn = document.getElementById("name");
+var txt = document.getElementById("welcome");
+txt.innerHTML = "Bienvenida"
+}
